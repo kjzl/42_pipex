9f96c6513a5f918343e38dfdb97174613b86162f
Kajus <kjzl@posteo.de>
2024-06-21 01:05:33 +0200

    Fix potential memory leak

--- cmd_str_to_argv.c
+++ cmd_str_to_argv.c
@@ -6,11 +6,23 @@
 /*   By: kwurster <kwurster@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 11:22:40 by kwurster          #+#    #+#             */
-/*   Updated: 2024/06/15 07:37:15 by kwurster         ###   ########.fr       */
+/*   Updated: 2024/06/21 01:04:52 by kwurster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+static t_bool	strsls_to_cloned_cstrs_and_cleanup(t_vec *split_cmd,
+					t_str *cmd_file)
+{
+	if (!vec_strsls_to_cloned_cstrs(split_cmd, split_cmd))
+	{
+		vec_destroy(split_cmd, 0);
+		str_destroy(cmd_file);
+		return (false);
+	}
+	return (true);
+}
 
 /// @brief Transforms a command string into an argv array
 /// (while also finding the full path to the cmd).
@@ -21,6 +33,7 @@ char	**cmd_str_to_argv(t_str_slice cmd, char *const *envp)
 {
 	t_vec	split_cmd;
 	t_str	cmd_file;
+	char	**out;
 
 	split_cmd = strsl_split(cmd, cstr_slice(" ", 1));
 	if (split_cmd.mem_err
@@ -31,17 +44,16 @@ char	**cmd_str_to_argv(t_str_slice cmd, char *const *envp)
 		return (0);
 	}
 	*(t_str_slice *)vec_get(&split_cmd) = str_view(&cmd_file);
-	if (!vec_strsls_to_cloned_cstrs(&split_cmd, &split_cmd))
-	{
-		vec_destroy(&split_cmd, 0);
-		str_destroy(&cmd_file);
+	if (!strsls_to_cloned_cstrs_and_cleanup(&split_cmd, &cmd_file))
 		return (0);
-	}
 	str_destroy(&cmd_file);
 	if (!vec_push_null(&split_cmd))
 	{
 		vec_destroy(&split_cmd, iter_ptr_free);
 		return (0);
 	}
-	return (vec_take(&split_cmd));
+	out = vec_take(&split_cmd);
+	if (!out)
+		vec_destroy(&split_cmd, iter_ptr_free);
+	return (out);
 }
